12d7f065619fadf36a7168652c605c1d0cc4c50b
JingYi120 <l2898168@yahoo.com.tw>
2023-06-13 21:05:59 +0800

    feat: category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 const categoryController = require('../../controllers/category-controller')
 
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
 router.get('/clothes', adminController.getClothes)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
