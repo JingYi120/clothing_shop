876a5ba6d812e85a8f3a2bd97971540ef30d778c
JingYi120 <l2898168@yahoo.com.tw>
2023-06-13 16:15:27 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const clotheController = require('../controllers/clothe-controller')
+const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+
 router.get('/clothes', clotheController.getClothes)
 router.use('/', (req, res) => res.redirect('/clothes'))
 
