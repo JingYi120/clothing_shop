acc6c2127a42ed6fb75c4195ccff8cd1d4c45aba
JingYi120 <l2898168@yahoo.com.tw>
2023-06-14 01:43:03 +0800

    feat: define assocition between clothe and category

---
+++ migrations/20230613173539-add-category-id-to-clothe.js
@@ -0,0 +1,16 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Clothes', 'category_id', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Categories',
+        key: 'id'
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Clothes', 'category_id')
+  }
+}
