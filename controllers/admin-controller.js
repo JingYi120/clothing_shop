6f2d0dcb4991829c460e7570ad7aad11a71c3d9c
JingYi120 <l2898168@yahoo.com.tw>
2023-06-13 22:03:25 +0800

    feat: create clothe

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,30 @@
+const { Clothe } = require('../models')
+
 const adminController = {
-  getClothes: (req, res) => {
-    return res.render('admin/clothes')
+  getClothes: (req, res, next) => {
+    Clothe.findAll({
+      raw: true
+    })
+      .then(clothes => res.render('admin/clothes', { clothes }))
+      .catch(err => next(err))
+  },
+  createClothe: (req, res) => {
+    return res.render('admin/create-clothe')
+  },
+  postClothe: (req, res, next) => {
+    const { name, description, price } = req.body
+    if (!name || !description || !price) throw new Error('All fields are required!')
+    Clothe.create({ 
+      name,
+      description,
+      price
+    })
+      .then(() => {
+        req.flash('success_messages', 'Item was successfully created')
+        res.redirect('/admin/clothes')
+      })
+      .catch(err => next(err))
   }
 }
+
 module.exports = adminController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,10 +3,13 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 const categoryController = require('../../controllers/category-controller')
 
+router.get('/clothes/create', adminController.createClothe)
+router.get('/clothes', adminController.getClothes)
+router.post('/clothes', adminController.postClothe)
+
 router.get('/categories/:id', categoryController.getCategories)
 router.put('/categories/:id', categoryController.putCategory)
 router.delete('/categories/:id', categoryController.deleteCategory)
-router.get('/clothes', adminController.getClothes)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 
