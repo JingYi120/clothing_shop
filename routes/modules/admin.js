9bb087af5a4e1173880677131c8b636feaed6bb8
JingYi120 <l2898168@yahoo.com.tw>
2023-06-13 20:48:26 +0800

    feat: add admin categories page & ifCond hbs helper

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
+const categoryController = require('../../controllers/category-controller')
 
 router.get('/clothes', adminController.getClothes)
+router.get('/categories', categoryController.getCategories)
 router.use('/', (req, res) => res.redirect('/admin/clothes'))
 
 module.exports = router
