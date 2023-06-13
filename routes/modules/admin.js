acfb828fd19ecdc289cb057ea8e2361038b968b7
JingYi120 <l2898168@yahoo.com.tw>
2023-06-13 19:34:34 +0800

    feat: user model add is_admin

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/clothes', adminController.getClothes)
-router.use('/', (req, res) => res.redirect('/admin/restaurants'))
+router.use('/', (req, res) => res.redirect('/admin/clothes'))
 
 module.exports = router
