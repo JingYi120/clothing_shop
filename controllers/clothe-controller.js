02adebf7881b7784769f0a5bd8c6215bce1758c1
JingYi120 <l2898168@yahoo.com.tw>
2023-06-17 23:24:11 +0800

    feat: add clothes index page

--- controllers/clothe-controller.js
+++ controllers/clothe-controller.js
@@ -1,6 +1,21 @@
+const { Clothe, Category, Image } = require('../models')
+
 const clotheController = {
-  getClothes: (req, res) => {
-    return res.render('clothes')
+  getClothes: (req, res, next) => {
+    return Clothe.findAll({
+      include: [Category, Image],
+      nest: true,
+      raw: true
+    }).then(clothes => {
+      const data = clothes.map(clothe => ({
+        ...clothe,
+        description: clothe.description.substring(0, 50)
+      }))
+      return res.render('clothes', {
+        clothes: data
+      })
+    }).catch(err => next(err))
   }
 }
+
 module.exports = clotheController
