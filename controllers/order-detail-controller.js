5e1a6d5f6d6b0cfc46f5a0e8be4ff48d44b8bf23
JingYi120 <l2898168@yahoo.com.tw>
2023-06-19 23:07:22 +0800

    feat : add orderDetails page

---
+++ controllers/order-detail-controller.js
@@ -0,0 +1,62 @@
+const { OrderDetail, Clothe, User, Order} = require('../models');
+const {getUser} = require('../helpers/auth-helpers')
+
+const orderDetailController = {
+  getOrderDetails: (req, res, next) => {
+    OrderDetail.findAll({
+      where: { orderId: null },
+      include: Clothe ,
+      nest: true,
+      raw: true
+    })
+    .then( orderDetails => {
+      const total = orderDetails.reduce((acc, o) => {
+        const price = Number(o.Clothe.price);
+        const quantity = Number(o.quantity);
+        return acc + price * quantity;
+      }, 0)
+
+      res.render('orderDetails', {
+        orderDetails,
+        total
+      })
+    })
+    .catch(err => next(err))
+  },
+  postOrderDetail: async (req, res, next) => {
+    try {
+      const { clotheId, quantity } = req.body;
+      const userId = getUser(req).id
+
+
+      const [user, clothe, orderDetails] = await Promise.all([User.findByPk(userId),
+        Clothe.findByPk(clotheId), 
+        OrderDetail.findOne({
+          where: { orderId: null, clotheId },
+          include: Clothe
+        })])
+
+
+      if (!user) throw new Error("User didn't exist!")
+      if (!clothe) throw new Error("Item didn't exist!")
+
+      if (orderDetails) {
+        orderDetails.quantity += Number(quantity);
+        await orderDetails.save();
+      } else {
+        await OrderDetail.create({
+          clotheId,
+          quantity,
+          userId
+        });
+      }
+
+      req.flash('success_messages', 'Add to cart!');
+      res.redirect(`/clothes/${clotheId}`);
+    } catch (err) {
+      next(err);
+    }
+  }
+};
+
+module.exports = orderDetailController;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const clotheController = require('../controllers/clothe-controller')
 const userController = require('../controllers/user-controller')
-// const orderController = require('../controllers/order-controller')
+const orderDetailController = require('../controllers/order-detail-controller')
 const passport = require('../config/passport')
 const admin = require('./modules/admin')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
@@ -16,6 +16,9 @@ router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
 
+router.get('/orderDetails', authenticated, orderDetailController.getOrderDetails)
+router.post('/orderDetails', authenticated, orderDetailController.postOrderDetail)
+
 router.patch('/users/order/:id', authenticated, userController.getOrder)
 router.get('/users/:id/orders', authenticated, userController.getOrders)
 router.get('/users/:id', authenticated, userController.getUser)
@@ -24,7 +27,7 @@ router.get('/users/:id', authenticated, userController.getUser)
 
 router.get('/clothes/:id', authenticated, clotheController.getClothe)
 router.get('/clothes', authenticated, clotheController.getClothes)
-
+router.use('/', (req, res) => res.redirect('/clothes'))
 router.use('/', generalErrorHandler)
 
 module.exports = router
