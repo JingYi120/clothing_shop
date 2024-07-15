fd57ee00a518eddf627d112f6235822c3e8a453b
JingYi120 <l2898168@yahoo.com.tw>
2024-07-16 01:12:04 +0800

    fix:favorite feature

--- public/favorite.js
+++ public/favorite.js
@@ -1,32 +1,31 @@
-const favoriteIcons = document.querySelectorAll('.favoriteIcon')
+const favoriteIcons = document.querySelectorAll(".favoriteIcon")
 
-favoriteIcons.forEach(icon => {
-  icon.addEventListener('click', () => {
-    const form = icon.closest('form')
-    const isFavorited = form.dataset.isFavorited === 'true'
-    const clotheId = form.getAttribute('action').split('/').pop()
+favoriteIcons.forEach((icon) => {
+  icon.addEventListener("click", (event) => {
+    event.preventDefault()
+    const form = icon.closest("form")
+    const isFavorited = form.dataset.isFavorited === "true"
+    const clotheId = form.getAttribute("action").split("/").pop()
 
-    const url = isFavorited ? `/favorite/${clotheId}?_method=DELETE` : `/favorite/${clotheId}`
-    const method = isFavorited ? 'DELETE' : 'POST'
+    const url = isFavorited ? `/favorite/${clotheId}?_method=DELETE`:`/favorite/${clotheId}`
+    const method = isFavorited ? "DELETE" : "POST"
 
     axios({
       url,
-      method
+      method,
     })
-      .then(response => {
+      .then((response) => {
         if (response.data.success) {
           if (isFavorited) {
-            form.dataset.isFavorited = 'false'
-            icon.style.color = '#b7bec8'
+            form.dataset.isFavorited = "false"
+            icon.style.color = "#b7bec8"
           } else {
-            form.dataset.isFavorited = 'true'
-            icon.style.color = '#d7370f'
+            form.dataset.isFavorited = "true"
+            icon.style.color = "#d7370f"
           }
-        } else {
-          console.error(response.data.message)
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error)
       })
   })
