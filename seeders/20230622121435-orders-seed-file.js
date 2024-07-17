5f2476e5044ef1d1a7f947e0eaf64ed195417040
JingYi120 <l2898168@yahoo.com.tw>
2024-07-17 23:30:19 +0800

    modify: orders, order details seeder

--- seeders/20230622121435-orders-seed-file.js
+++ seeders/20230622121435-orders-seed-file.js
@@ -1,19 +1,20 @@
-'use strict'
-const faker = require('faker');
+"use strict"
+const faker = require("faker")
+const dayjs = require("dayjs")
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const users = await queryInterface.sequelize.query(
-      'SELECT id FROM Users;',
-      { type: queryInterface.sequelize.QueryTypes.SELECT }
-    )
+    const users = await queryInterface.sequelize.query("SELECT id FROM Users;", {
+      type: queryInterface.sequelize.QueryTypes.SELECT,
+    })
 
-    await queryInterface.bulkInsert('Orders', [
+    await queryInterface.bulkInsert("Orders", [
       ...Array.from({ length: 3 }, (_, i) => ({
         user_id: users[i % users.length].id,
         is_done: false,
         is_order: false,
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
       })),
       ...Array.from({ length: 3 }, (_, i) => ({
         user_id: users[i % users.length].id,
@@ -21,19 +22,36 @@ module.exports = {
         is_done: false,
         is_order: true,
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
       })),
-      ...Array.from({ length: 9 }, (_, i) => ({
-        user_id: users[i % users.length].id,
-        note: faker.lorem.text(),
-        is_done: true,
-        is_order: true,
-        created_at: new Date(),
-        updated_at: new Date()
-      }))
+      ...Array.from({ length: 24 }, (_, i) => {
+        const currentYear = new Date().getFullYear()
+        const randomMonth = Math.floor(Math.random() * 12)
+        const randomDay = Math.floor(Math.random() * 28) + 1
+
+        const createdAt = dayjs()
+          .year(currentYear)
+          .month(randomMonth)
+          .date(randomDay)
+          .hour(Math.floor(Math.random() * 24))
+          .minute(Math.floor(Math.random() * 60))
+          .second(Math.floor(Math.random() * 60))
+          .toDate()
+
+        const updatedAt = dayjs(createdAt).add(1, "day").toDate()
+
+        return {
+          user_id: users[i % users.length].id,
+          note: faker.lorem.text(),
+          is_done: true,
+          is_order: true,
+          created_at: createdAt,
+          updated_at: updatedAt,
+        }
+      }),
     ])
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Orders', {})
-  }
+    await queryInterface.bulkDelete("Orders", {})
+  },
 }
--- seeders/20230622121455-orderDetails-seed-file.js
+++ seeders/20230622121455-orderDetails-seed-file.js
@@ -10,14 +10,25 @@ module.exports = {
       'SELECT id FROM Clothes;',
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
-    await queryInterface.bulkInsert('OrderDetails',[
-      ...Array.from({ length: 25 }, () => ({
+
+    const orderDetails = orders.map((order) => ({
+      order_id: order.id,
+      clothe_id: clothes[Math.floor(Math.random() * clothes.length)].id,
+      quantity: Math.floor(Math.random() * 3) + 1,
+      is_order: true,
+      created_at: new Date(),
+      updated_at: new Date(),
+    }))
+
+    await queryInterface.bulkInsert("OrderDetails", [
+      ...orderDetails,
+      ...Array.from({ length: 10 }, () => ({
         order_id: orders[Math.floor(Math.random() * orders.length)].id,
         clothe_id: clothes[Math.floor(Math.random() * clothes.length)].id,
-        quantity: 1,
+        quantity: Math.floor(Math.random() * 2) + 1,
         is_order: true,
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
       })),
       ...Array.from({ length: 7 }, () => ({
         order_id: orders[Math.floor(Math.random() * orders.length)].id,
@@ -25,8 +36,8 @@ module.exports = {
         quantity: 1,
         is_order: false,
         created_at: new Date(),
-        updated_at: new Date()
-      }))
+        updated_at: new Date(),
+      })),
     ])
   },
   down: async (queryInterface, Sequelize) => {
